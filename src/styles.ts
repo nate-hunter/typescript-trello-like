3544bb6a401c0d8520f56410fa7227840bb46bdb
Nate Hunter <hunter.j.nate@gmail.com>
2021-11-30 21:10:01 -0500

    added styles for adding new items

--- src/styles.ts
+++ src/styles.ts
@@ -34,3 +34,54 @@ export const CardContainer = styled.div`
   border-radius: 3px;
   box-shadow: #091e4240 0px 1px 0px 0px;
 `;
+
+type AddItemButtonProps = {
+  dark?: boolean
+}
+
+export const AddItemButton = styled.button<AddItemButtonProps>`
+  background-color: #e6e6e6;
+  border-radius: 3px;
+  border: none;
+  color: ${props => (props.dark ? '#000000' : '#ffffff' )};
+  cursor: pointer;
+  max-width: 300px;
+  padding: 10px 12px;
+  text-align: left;
+  transition: background 85ms ease-in;
+  width: 100%;
+  &:hover {
+    background-color: #e6e6e6;
+  }
+`;
+
+export const NewItemFormContainer = styled.div`
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+`;
+
+export const NewItemButton = styled.button`
+  background-color: #6565f6;
+  border-radius: 3px;
+  border: none;
+  box-shadow: none;
+  color: #ffffff;
+  padding: 6px 12px;
+  text-align: center;
+`;
+
+export const NewItemInput = styled.input`
+  border-radius: 3px;
+  border: none;
+  box-shadow: #9595f9 0px 1px 0px 0px;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+`;
+
+
+
+
